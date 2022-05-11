0a71a465e516ee851b239ea7e58d2163975b42f8
Anil Nanda <anilthekkoote@gmail.com>
2022-05-11 15:37:24 +0400

    initial commit

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.11.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
+
+resource "aws_iam_user" "lb" {
+  name = "lbUser"
+  tags = {
+	"Inactivate" = "True"
+}
+}
